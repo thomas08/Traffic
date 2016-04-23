b6085e203e78c59627388378ce2e1f1120b3edda
thomas <wetsiri@gmail.com>
2016-04-23 15:05:55 +0700

    start app traffic 03 finish

--- Traffic/app/src/main/java/info/innovation_startup/traffic/MainActivity.java
+++ Traffic/app/src/main/java/info/innovation_startup/traffic/MainActivity.java
@@ -1,13 +1,51 @@
 package info.innovation_startup.traffic;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit ประกาศตัวแปร
+    private ListView trafficListView;
+    private Button aboutButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+       //Bind widget
+        trafficListView = (ListView) findViewById(R.id.listView);
+        aboutButton = (Button) findViewById(R.id.button);
+
+        //Button controller
+        buttonControtter();
+
+
+
+    } //Main Method
+
+    private void buttonControtter() {
+
+        aboutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                //Sound
+                MediaPlayer mediaPlayer = MediaPlayer.create(getBaseContext(),R.raw.horse);
+                mediaPlayer.start();
+
+
+            } //onclick
+        });
+
+    } // Button controller Method
+
+
+}  //Main class
