b4b6fbd1c311e27d620180621a4f22f05eab7487
thomas <wetsiri@gmail.com>
2016-04-24 11:51:30 +0700

    start app traffic adater

---
+++ Traffic/app/src/main/java/info/innovation_startup/traffic/Adapter.java
@@ -0,0 +1,59 @@
+package info.innovation_startup.traffic;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+/**
+ * Created by tossaporn on 4/24/16 AD.
+ */
+public class Adapter extends BaseAdapter{
+
+    //Explicit
+    private Context context;
+    private int[] iconInts;
+    private String[] titleStrins, detailStrings;
+
+    public Adapter(Context context, int[] iconInts, String[] titleStrins, String[] detailStrings) {
+        this.context = context;
+        this.iconInts = iconInts;
+        this.titleStrins = titleStrins;
+        this.detailStrings = detailStrings;
+    }
+
+    @Override
+    public int getCount() {
+        return iconInts.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View convertView, ViewGroup parent) {
+
+        LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(R.layout.my_listview,ViewGroup, false);
+
+        //Show text
+        TextView titleTextView = (TextView) view.findViewById(R.id.textView);
+        titleTextView.setText(titleStrins[i]);
+
+        TextView detailTextView = (TextView) view.findViewById(R.id.textView2);
+        detailTextView.setText(detailStrings[i]);
+
+
+
+        return view;
+    }
+} //Main
