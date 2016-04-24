9e49e78537762ac050cb92225f963863338db5d9
thomas <wetsiri@gmail.com>
2016-04-24 10:14:55 +0700

    start app traffic 04

--- Traffic/app/src/main/java/info/innovation_startup/traffic/MainActivity.java
+++ Traffic/app/src/main/java/info/innovation_startup/traffic/MainActivity.java
@@ -1,17 +1,28 @@
 package info.innovation_startup.traffic;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 public class MainActivity extends AppCompatActivity {
 
     //Explicit ประกาศตัวแปร
     private ListView trafficListView;
     private Button aboutButton;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
 
     @Override
@@ -19,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-       //Bind widget
+        //Bind widget
         trafficListView = (ListView) findViewById(R.id.listView);
         aboutButton = (Button) findViewById(R.id.button);
 
@@ -27,7 +38,9 @@ public class MainActivity extends AppCompatActivity {
         buttonControtter();
 
 
-
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     } //Main Method
 
     private void buttonControtter() {
@@ -38,11 +51,18 @@ public class MainActivity extends AppCompatActivity {
 
 
                 //Sound
-                MediaPlayer mediaPlayer = MediaPlayer.create(getBaseContext(),R.raw.horse);
+                MediaPlayer mediaPlayer = MediaPlayer.create(getBaseContext(), R.raw.horse);
                 mediaPlayer.start();
 
 
-
+                //webview
+//                Intent intent = new Intent(Intent.ACTION_VIEW);
+//                intent.setData(Uri.parse("https://youtu.be/WZoYSMKy2ME"));
+//                startActivity(intent);
+                //call
+                Intent intent = new Intent(Intent.ACTION_CALL);
+                intent.setData(Uri.parse("tel:0814855783"));
+                startActivity(intent);
 
             } //onclick
         });
@@ -50,4 +70,43 @@ public class MainActivity extends AppCompatActivity {
     } // Button controller Method
 
 
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://info.innovation_startup.traffic/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://info.innovation_startup.traffic/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
 }  //Main class
